78fbe71e1dc063bc4072db2910595a8de507e9e0
fertuquerres <56898229+fertuquerres@users.noreply.github.com>
2019-11-12 16:03:56 -0500

    Add files via upload

---
+++ Futbolista.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Futbolista
+{
+    public class Futbolista
+    {
+        private string nombre;
+        private string equipo;
+        private int edad;
+        private string nacionalidad;
+
+        public Futbolista()
+        {
+            nombre = "";
+            equipo = "";
+            edad = 0;
+            nacionalidad = "";
+        }
+        public Futbolista(string nnombre,string nequipo,int nedad,string nnacionalidad)
+        {
+            nombre = nnombre;
+            equipo = nequipo;
+            edad = nedad;
+            nacionalidad = nnacionalidad;
+        }
+        public void SetNombre(string nnombre)
+        {
+            nombre = nnombre;
+        }
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetEquipo(string nequipo)
+        {
+            equipo = nequipo;
+        }
+        public string GetEquipo()
+        {
+            return equipo;
+        }
+        public void SetEdad(int nedad)
+        {
+            edad = nedad;
+        }
+        public int GetEdad()
+        {
+            return edad;
+        }
+        public void SetNacionalidad(string nnacionalidad)
+        {
+            nacionalidad = nnacionalidad;
+        }
+        public string GetNacionalidad()
+        {
+            return nacionalidad;
+        }
+        public void ComprobarEdad()
+        {
+            if (edad%2==0)
+            {
+                Console.WriteLine("La edad del futbolista es par.");
+            }
+            else
+            {
+                Console.WriteLine("La edad del futbolista es impar.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Futbolista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Futbolista miFutbolista = new Futbolista();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Objeto 1.");
+            Console.WriteLine("----------------------");
+            Console.Write("Ingrese el nombre del futbolista: ");
+            miFutbolista.SetNombre(Console.ReadLine());
+            Console.Write("Ingrese el equipo del futbolista: ");
+            miFutbolista.SetEquipo(Console.ReadLine());
+            Console.Write("Ingrese la edad del futbolista: ");
+            miFutbolista.SetEdad(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Ingrese la nacionalidad del futbolista: ");
+            miFutbolista.SetNacionalidad(Console.ReadLine());
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Nombre: " + miFutbolista.GetNombre());
+            Console.WriteLine("Equipo: " + miFutbolista.GetEquipo());
+            Console.WriteLine("Edad: " + miFutbolista.GetEdad());
+            Console.WriteLine("Nacionalidad: " + miFutbolista.GetNacionalidad());
+            miFutbolista.ComprobarEdad();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Objeto 2");
+            Console.WriteLine("----------------------");
+            Futbolista miFutbolista1 = new Futbolista("Alberto Ordoñez", "Delfin FC", 33, "Ecuatoriana");
+            Console.WriteLine("Nombre: " + miFutbolista1.GetNombre());
+            Console.WriteLine("Equipo: " + miFutbolista1.GetEquipo());
+            Console.WriteLine("Edad: " + miFutbolista1.GetEdad());
+            Console.WriteLine("Nacionalidad: " + miFutbolista1.GetNacionalidad());
+            miFutbolista1.ComprobarEdad();
+            Console.ReadKey();
+        }
+    }
+}
